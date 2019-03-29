5316ab641f4b5baa875af978010e47403ef5a3db
HOIHOIHOII <bsailcloth@gmail.com>
2019-03-29 12:24:13 +1000

    added charts

--- basic - 1 machine, 3 states sim.py
+++ basic - 1 machine, 3 states sim.py
@@ -11,10 +11,11 @@ Created on Fri Mar 15 09:54:32 2019
 from __future__ import division
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import random
 
 
-random.seed(1)
+
 
 #state transition matrix
 #all rowsums = 1
@@ -58,18 +59,34 @@ def run_trial(initial_state, k=100):
         data += [result]
     return data
 
-def compute_pull_avg(trial):
-    initial_data = np.array(trial[1:])
-    n = initial_data.shape[0]
-    reward = initial_data[:,1].sum()
-#    print(initial_data[:,1])
-#    print(n)
-    return reward/n
+#begin script
 
-trial_data = run_trial(initial_state_and_reward,100000)
+k=1000
 
-compute_pull_avg(trial_data)
+for _ in (1,2):
+    random.seed(1)
+    
+    trial_data = run_trial(initial_state_and_reward,k)
+    
+    trial_data = pd.DataFrame(data = trial_data[1:], columns = trial_data[0])
+    trial_data["Running_Total"] = trial_data.Reward.expanding().sum()
+    trial_data["Running_Average"] = trial_data.Reward.expanding().mean()
+    
+    avg = trial_data.iloc[-1]["Running_Average"]
+    print("The long term average over {} trials is: {}".format(k,avg))
+    
+    fig, (ax1,ax2) = plt.subplots(2,1)
+    
+    ax1.plot(trial_data.Running_Average)
+    ax1.plot(range(k),[avg for _ in range(k)])
+    
+    ax2.plot(trial_data.loc[k-100:,"Running_Average"])
+    ax2.plot(range(k-100,k),[avg for _ in range(k-100,k)])
+    plt.draw()
 
-trial_data = pd.DataFrame(data = trial_data[1:], columns = trial_data[0])
-
-trial_data
+#observation: Compare the two plots. 
+#1. The average reward approximately converges over a relatively small number 
+#    of trials, but
+#2. fine convergence takes a very long time, and
+#3. there is a significant variation in the long term average, even for the same
+#   seed. (I think this must be due to a subtle error)
